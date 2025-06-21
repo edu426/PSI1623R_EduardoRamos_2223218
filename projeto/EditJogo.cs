f7bcf200eac97cd75de6950d4da6c18d30dfccaf
Eduardo Ramos <eduardopramos30@gmail.com>
2025-06-21 12:11:34 +0100

    Game update test

--- projeto/EditJogo.cs
+++ projeto/EditJogo.cs
@@ -82,6 +82,63 @@ namespace projeto
 
         private void btnCon_Click(object sender, EventArgs e)
         {
-        }
+            string nome = "", gamePic = "";
+            int genId = 0;
+
+            nome = txtNome.Text;
+
+            HttpVerify httpVerify = new HttpVerify();
+            gamePic = httpVerify.Verify(txtImage.Text);
+
+            genId = (int)((DataRowView)comboGen.SelectedItem)["IdGenero"];
+
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    if (nome == "" || gamePic == "ERr0r404")
+                    {
+                        MessageBox.Show("Valores Inválidos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        conn.Open();
+
+                        string query = "SELECT nome FROM Utilizador WHERE nome = @nome";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@nome", nome);
+
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        if (dr.HasRows)
+                        {
+                            MessageBox.Show("Já existe um utilizador com esse nome!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                        }
+                        else
+                        {
+                            dr.Close();
+
+                            query = "UPDATE Utilizador SET nome=@nome WHERE idutilizador =@id";
+
+                            SqlCommand cmd2 = new SqlCommand(query, conn);
+
+                            cmd2.Parameters.AddWithValue("@nome", nome);
+                            cmd2.Parameters.AddWithValue("id", id);
+                            cmd2.ExecuteNonQuery();
+
+                            MessageBox.Show("Nome atulizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Não foi possível ligar a base de dados!\n{ex}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
     }
 }
